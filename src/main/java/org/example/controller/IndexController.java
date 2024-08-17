5a850dfb6fee57ea3d6ef04677abb9c9df365051
gg <gg@gmail.com>
2024-08-17 22:37:07 +0800

    pom

---
+++ src/main/java/org/example/controller/IndexController.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class IndexController {
+    @GetMapping("/time")
+    public LocalDateTime time(){
+        return LocalDateTime.now();
+    }
+}
