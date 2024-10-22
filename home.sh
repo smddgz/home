f3cde503223bec167fe936884699879ecfe184d3
gg <gg@gmail.com>
2024-10-22 21:02:33 +0800

    test

--- home.sh
+++ home.sh
@@ -1,4 +1,6 @@
-cd /root/home
+cd /root/home || exit
 git pull
 mvn package
-systemctl restart home
+port=$(lsof -i:80|awk 'NR==2 {print $2}')
+kill -9 "$port"
+nohup java -jar /root/home/target/home.jar &
